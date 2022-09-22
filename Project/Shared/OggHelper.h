0c34d9954982d65b3cc97ea15ccec94c6bfd3b90
Marcelo Sarquis <marcelosarquis@hotmail.com>
2022-09-22 21:26:22 +0200

    add watchOS scripts

--- Project/Shared/OggHelper.h
+++ Project/Shared/OggHelper.h
@@ -7,5 +7,3 @@
 - (NSMutableData *)writePacket:(NSData*)data frameSize:(int)frameSize;
 
 @end
-
-// warning build: Building for iOS Simulator, but linking in object file (/Users/marcelosarquis/Library/Developer/Xcode/DerivedData/BLOpus-fmmioyodpixmnjgipiwmaggeqlrm/Build/Intermediates.noindex/BLOpus.build/Debug-iphonesimulator/lib-opus-ios.build/Objects-normal/arm64/lib_opus_ios_lto.o) built for iOS
