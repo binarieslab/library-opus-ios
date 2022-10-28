30668096014217b9bf426b9a7271ba3ed2c6e339
Marcelo Sarquis <marcelosarquis@hotmail.com>
2022-10-28 08:32:59 +0200

    add OpusSwift tests

--- OpusSwiftTests/OpusSwiftTests.swift
+++ OpusSwiftTests/OpusSwiftTests.swift
@@ -5,6 +5,7 @@
 //  Created by Marcelo Sarquis on 27.10.22.
 //
 
+@testable import OpusSwift
 import XCTest
 
 final class OpusSwiftTests: XCTestCase {
@@ -17,19 +18,67 @@ final class OpusSwiftTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testEncoder_whenValidAudioIsGiven_shouldEncodeNewFormat() throws {
+        // Given
+        let pcmData = TestUtils.pcmfile.data
+        let pcmRate: Int32 = 48000
+        let pcmChannels: Int32 = 1
+        let frameSize: UInt32 = pcmData.count.calculateFrameSize(forOriginalFrame: 960)
+        let opusRate: Int32 = 48000
+        
+        // When
+        let encoder = try OpusEncoder(pcmRate: pcmRate, pcmChannels: pcmChannels, pcmBytesPerFrame: frameSize, opusRate: opusRate, application: .audio)
+        let encodedData = try encoder.encode(pcm: pcmData) // TODO !!!
+        
+        // Expected
+        let encodedURL = TestUtils.pcmfile.url
+            .deletingLastPathComponent()
+            .appendingPathComponent("encodedfile.ogg")
+        try encodedData.write(to: encodedURL)
+        
+        XCTAssertTrue(encodedData.count > 0)
     }
+    
+    func testEncoder_whenWrongAudioIsGiven_shouldThrowError() throws {
+    }
+    
+    func testDecoder_whenValidAudioIsGiven_shouldEncodeNewFormat() throws {
+        // Given
+        let audioData = TestUtils.opusfile.data
+        let numChannels: Int32 = 1
+        let sampleRate: Int32 = 48000
+        
+        // When
+        let decoder = try OpusDecoder(numChannels: numChannels, sampleRate: sampleRate)
+        let decodedData = try decoder.decode(audioData: audioData)
+        
+        // Expected
+        XCTAssertEqual(decodedData.count, 283892)
+    }
+    
+    func testDecoder_whenWrongAudioIsGiven_shouldThrowError() throws {
+    }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+private extension Int {
+    func calculateFrameSize(forOriginalFrame frame: UInt32) -> UInt32 {
+        var upperPcmBytesPerFrame = frame
+        while true {
+            if UInt32(self) % upperPcmBytesPerFrame == 0 {
+                return upperPcmBytesPerFrame
+            }
+            if upperPcmBytesPerFrame < UInt32(self) {
+                break
+            }
+            upperPcmBytesPerFrame += 1
+        }
+        
+        var downerPcmBytesPerFrame = frame
+        while true {
+            if UInt32(self) % downerPcmBytesPerFrame == 0 {
+                return downerPcmBytesPerFrame
+            }
+            downerPcmBytesPerFrame -= 1
         }
     }
-
 }
